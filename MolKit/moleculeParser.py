9a41978e4c1d050a01577af6277d788e371e0261
MARIO <mariosergiovaldes145@gmail.com>
2020-05-03 02:19:41 -0500

    updated moleculeParser

--- MolKit/moleculeParser.py
+++ MolKit/moleculeParser.py
@@ -9,7 +9,7 @@
 #  Please use this cite the original reference.                                                    #
 #  If you think my work helps you, just keep this note intact on your program.                     #
 #                                                                                                  #
-#  Modification date: 28/8/19 4:40                                                                 #
+#  Modification date: 3/5/20 2:19                                                                  #
 #                                                                                                  #
 # ##################################################################################################
 
@@ -21,25 +21,17 @@
 #
 #############################################################################
 
-#
-# $Header: /opt/cvs/python/packages/share1.5/MolKit/moleculeParser.py,v 1.9.8.2 2017/02/08 21:49:34 forli Exp $
-#
-# $Id: moleculeParser.py,v 1.9.8.2 2017/02/08 21:49:34 forli Exp $
-#
-
-from MolKit.molecule import Atom
-import warnings, os
-from mglutil.util.misc import ensureFontCase
+import warnings
 
 # dict used to guess parser based on file extension
-parserToExt = {'PDB':'.pdb', 'PDBQ':'.pdbq',
-               'PDBQS':'.pdbqs', 'PDBQT':'.pdbqt',
-               'MOL2':'.mol2',
-               'PQR':'.pqr',
-               'GRO':'.gro',
-               'F2D':'.f2d',
-               'SDF':'.sdf',
-               'CIF':'.cif',
+parserToExt = {'PDB': '.pdb', 'PDBQ': '.pdbq',
+               'PDBQS': '.pdbqs', 'PDBQT': '.pdbqt',
+               'MOL2': '.mol2',
+               'PQR': '.pqr',
+               'GRO': '.gro',
+               'F2D': '.f2d',
+               'SDF': '.sdf',
+               'CIF': '.cif',
                }
 
 
@@ -49,30 +41,15 @@ class MoleculeParser:
         supply the lines directly via allLines
         """
         self.filename = str(filename)
-        self.allLines = allLines #stores all lines from file
-
+        self.allLines = allLines  # stores all lines from file
 
     def readFile(self):
         f = open(self.filename)
         self.allLines = f.readlines()
-        if len(self.allLines)==1:
+        if len(self.allLines) == 1:
             # this file probably has \r instead or \n
             self.allLines = self.allLines[0].split('\r')
             warnings.warn('Only 1 line read from PDB file, splitting on \r')
         f.close()
-        self.allLines = list(filter( lambda x,s=str.strip: len(s(x)),
-                                self.allLines ))
-
-
-    def viewSource(self):
-        import tkinter, Pmw
-        root = tkinter.Toplevel()
-        root.title(self.filename)
-        self.st = Pmw.ScrolledText(root)
-        self.st.pack(fill = 'both', expand=1)
-
-        self.st._textbox.configure(bg='white', font=(ensureFontCase('Courier'), '10'))
-        txt = ''
-        for line in self.allLines:
-            txt += ''.join(line)
-        self.st.setvalue(txt)
+        self.allLines = list(filter(lambda x, s=str.strip: len(s(x)),
+                                    self.allLines))
