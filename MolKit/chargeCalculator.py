a3e103f96579b6768cf4fd6956a023a2c40fbb28
MARIO <mariosergiovaldes145@gmail.com>
2020-05-03 23:12:19 -0500

    updated MolKit > chargeCalculator.py

--- MolKit/chargeCalculator.py
+++ MolKit/chargeCalculator.py
@@ -9,7 +9,7 @@
 #  Please use this cite the original reference.                                                    #
 #  If you think my work helps you, just keep this note intact on your program.                     #
 #                                                                                                  #
-#  Modification date: 28/8/19 4:40                                                                 #
+#  Modification date: 3/5/20 23:12                                                                 #
 #                                                                                                  #
 # ##################################################################################################
 
@@ -21,24 +21,17 @@
 #
 #############################################################################
 
-#
-# $Header: /opt/cvs/python/packages/share1.5/MolKit/chargeCalculator.py,v 1.5 2004/08/11 15:17:53 rhuey Exp $
-#
-# $Id: chargeCalculator.py,v 1.5 2004/08/11 15:17:53 rhuey Exp $
-#
-
 """
 This module implements the ChargeCalculators classes which addCharges of 
 various sorts to AtomSets.
 """
 
-from MolKit.molecule import Atom, AtomSet, BondSet
+# this is for future use
+# from AutoDockTools.observer import Subject
+# FIX THIS: it shouldn't be in AutoDockTools
+from MolKit.data.qkollua import q
 from PyBabel.atomTypes import AtomHybridization
 from PyBabel.gasteiger import Gasteiger
-#this is for future use
-#from AutoDockTools.observer import Subject
-#FIX THIS: it shouldn't be in AutoDockTools
-from MolKit.data.qkollua import q
 
 
 class ChargeCalculator:
@@ -49,11 +42,9 @@ atom._charge dictionary and setting the atom.chargeSet to self.chtype
     def __init__(self, chtype=''):
         self.chtype = ''
 
-
     def addCharges(self, atoms):
-        #method to override
+        # method to override
         pass
-
 
 
 class GasteigerChargeCalculator(ChargeCalculator):
@@ -75,7 +66,6 @@ each of the oxygens bonded to the phosphorus atom.
         ChargeCalculator(self)
         self.chtype = 'gasteiger'
 
-
     def addCharges(self, atoms):
         """
         compute gasteiger charges, add them to atoms, return list of new
@@ -86,26 +76,24 @@ each of the oxygens bonded to the phosphorus atom.
         atom has a babel type
 
         """
-        #print 'in computeGasteiger'
-        #check that atoms have babel_types
+        # print 'in computeGasteiger'
+        # check that atoms have babel_types
         w_babel_types = atoms.get(lambda x: hasattr(x, 'babel_type'))
-        if w_babel_types is None or len(w_babel_types)!=len(atoms):
+        if w_babel_types is None or len(w_babel_types) != len(atoms):
             babel = AtomHybridization()
-            #have to have bonds for this
+            # have to have bonds for this
             babel.assignHybridization(atoms)
         Gast = Gasteiger()
         Gast.compute(atoms)
         gastCharges = []
         for c in atoms.gast_charge:
             gastCharges.append(round(c, 4))
-        #NB THIS adds entry 'gasteiger' to atom._charges dict
+        # NB THIS adds entry 'gasteiger' to atom._charges dict
         atoms.addCharges('gasteiger', gastCharges)
         atoms.chargeSet = 'gasteiger'
-        #clean-up
+        # clean-up
         delattr(atoms, 'gast_charge')
         return gastCharges
-            
-
 
 
 class KollmanChargeCalculator(ChargeCalculator):
@@ -118,9 +106,8 @@ atom._charge dictionary and to set the atom.chargeSet to self.chtype
         self.chtype = 'Kollman'
         self.q = q
 
-
     def addCharges(self, atoms):
-        #print 'in KollmanChargeCalculator.addCharges'
+        # print 'in KollmanChargeCalculator.addCharges'
         for a in atoms:
             if a.parent.type not in self.q:
                 a._charges['Kollman'] = 0.0
@@ -131,19 +118,19 @@ atom._charge dictionary and to set the atom.chargeSet to self.chtype
                 else:
                     a._charges['Kollman'] = 0.0
             a.chargeSet = 'Kollman'
-        #truncated here... should
+        # truncated here... should
         allRes = atoms.parent.uniq()
-        #fix histidines
-        hisRes = allRes.get(lambda x:x.name[:3]=='HIS')
+        # fix histidines
+        hisRes = allRes.get(lambda x: x.name[:3] == 'HIS')
         if hisRes is not None and len(hisRes):
             for hres in hisRes:
                 self.fixHisRes(hres)
-        #fix cysteines
-        cysRes = allRes.get(lambda x:x.name[:3]=='CYS')
+        # fix cysteines
+        cysRes = allRes.get(lambda x: x.name[:3] == 'CYS')
         if cysRes is not None and len(cysRes):
             for cres in cysRes:
                 self.fixCysRes(cres)
-        #fix termini
+        # fix termini
         allChains = allRes.parent.uniq()
         for ch in allChains:
             if ch.residues[0] in allRes:
@@ -151,16 +138,15 @@ atom._charge dictionary and to set the atom.chargeSet to self.chtype
                 self.fixNterminus(ch.residues[0])
             if ch.residues[-1] in allRes:
                 self.fixCterminus(ch.residues[-1])
-        #make Kollman the current charge
+        # make Kollman the current charge
         self.chargeSet = 'Kollman'
         return atoms.charge
-
 
     def fixNterminus(self, nres):
         """newTotal<-fixNterminu(nres)
             nres is the N-terminal residue
         """
-        nresSet =  nres.atoms.get('N')
+        nresSet = nres.atoms.get('N')
         if nresSet is None or not len(nresSet):
             if nres.type in self.q:
                 for at in nres.atoms:
@@ -172,7 +158,7 @@ atom._charge dictionary and to set the atom.chargeSet to self.chtype
                 print(nres.name, ' not in qkollua; charges set to 0.0')
                 for at in nres.atoms:
                     at._charges['Kollman'] = 0.0
-            return 
+            return
         Natom = nres.atoms.get('N')[0]
         caresSet = nres.atoms.get('CA')
         if caresSet is None or not len(caresSet):
@@ -183,17 +169,17 @@ atom._charge dictionary and to set the atom.chargeSet to self.chtype
                 print(nres.name, ' not in qkollua; charges set to 0.0')
                 for at in nres.atoms:
                     at._charges['Kollman'] = 0.0
-            return 
+            return
         CAatom = nres.atoms.get('CA')[0]
         hlist = Natom.findHydrogens()
-        #5/5:assert len(hlist), 'polar hydrogens missing from n-terminus'
-        if not len(hlist): 
+        # 5/5:assert len(hlist), 'polar hydrogens missing from n-terminus'
+        if not len(hlist):
             print('polar hydrogens missing from n-terminus of chain ' + nres.parent.name)
-            #self.vf.warningMsg('polar hydrogens missing from n-terminus')
+            # self.vf.warningMsg('polar hydrogens missing from n-terminus')
         if nres.type == 'PRO':
-            #divide .059 additional charge between CA + CD
-            #FIX THIS what if no CD?
-            #CDatom = nres.atoms.get('CD')[0]
+            # divide .059 additional charge between CA + CD
+            # FIX THIS what if no CD?
+            # CDatom = nres.atoms.get('CD')[0]
             CDatom = nres.atoms.get('CD')
             if CDatom is not None:
                 CDatom = CDatom[0]
@@ -209,9 +195,8 @@ atom._charge dictionary and to set the atom.chargeSet to self.chtype
             CAatom._charges['Kollman'] = CAatom._charges['Kollman'] + .055
             for ha in hlist:
                 ha._charges['Kollman'] = .312
-            
-        return 
 
+        return
 
     def fixCterminus(self, cres):
         """newTotal<-fixCterminu(cres)
@@ -222,95 +207,88 @@ atom._charge dictionary and to set the atom.chargeSet to self.chtype
         if OXYatomSet is not None and len(OXYatomSet):
             OXYatom = OXYatomSet[0]
             OXYatom._charges['Kollman'] = -.706
-            #CAUTION!
+            # CAUTION!
             CAatom = cres.atoms.get('CA')[0]
             CAatom._charges['Kollman'] = CAatom._charges['Kollman'] - .006
-            #CAUTION!
+            # CAUTION!
             Catom = cres.atoms.get('C')[0]
             Catom._charges['Kollman'] = Catom._charges['Kollman'] - .082
-            #CAUTION!
+            # CAUTION!
             Oatom = cres.atoms.get('O')[0]
             Oatom._charges['Kollman'] = Oatom._charges['Kollman'] - .206
-            return 
+            return
         elif OCTatomSet is not None and len(OCTatomSet):
             OXYatom = OCTatomSet[0]
             OXYatom._charges['Kollman'] = -.706
-            #CAUTION!
+            # CAUTION!
             CAatom = cres.atoms.get('CA')[0]
             CAatom._charges['Kollman'] = CAatom._charges['Kollman'] - .006
-            #CAUTION!
+            # CAUTION!
             Catom = cres.atoms.get('C')[0]
             Catom._charges['Kollman'] = Catom._charges['Kollman'] - .082
-            #CAUTION!
+            # CAUTION!
             Oatom = cres.atoms.get('O')[0]
             Oatom._charges['Kollman'] = Oatom._charges['Kollman'] - .206
-            return 
-            
-
-
+            return
 
     def fixHisRes(self, his):
         """newTotal<-fixHisRes(his)
             his is a HISTIDINE residue
         """
         hisAtomNames = his.atoms.name
-        #oldcharge = Numeric.sum(his.atoms._charges['Kollman'])
+        # oldcharge = Numeric.sum(his.atoms._charges['Kollman'])
         oldcharge = 0
         for at in his.atoms:
             oldcharge = oldcharge + at._charges['Kollman']
         assertStr = his.name + ' is lacking polar hydrogens'
         assert 'HD1' or 'HE2' in hisAtomNames, assertStr
-        #get the appropriate dictionary
+        # get the appropriate dictionary
         if 'HD1' in hisAtomNames and 'HE2' in hisAtomNames:
             d = q['HIS+']
-        elif 'HD1' in  hisAtomNames:
+        elif 'HD1' in hisAtomNames:
             d = q['HISD']
         elif 'HE2' in hisAtomNames:
             d = q['HIS']
         else:
             msgStr = his.full_name() + ' missing both hydrogens!'
             print(msgStr)
-            return 
+            return
 
-        #assign charges
+            # assign charges
         for a in his.atoms:
             if a.name in d:
                 a._charges['Kollman'] = d[a.name]
             else:
                 a._charges['Kollman'] = 0.0
 
-        #correct total
-        #newcharge = Numeric.sum(his.atoms._charges['Kollman'])
+        # correct total
+        # newcharge = Numeric.sum(his.atoms._charges['Kollman'])
         newcharge = 0
         for at in his.atoms:
             newcharge = newcharge + at._charges['Kollman']
 
-
     def fixCysRes(self, cys):
         cysAtomNames = cys.atoms.name
-        #oldcharge = Numeric.sum(cys.atoms._charges['Kollman'])
+        # oldcharge = Numeric.sum(cys.atoms._charges['Kollman'])
         oldcharge = 0
         for at in cys.atoms:
             oldcharge = oldcharge + at._charges['Kollman']
-        #get the appropriate dictionary
+        # get the appropriate dictionary
         if 'HG' in cysAtomNames:
             d = self.q['CYSH']
         else:
-            #cystine
+            # cystine
             d = self.q['CYS']
 
-        #assign charges
+        # assign charges
         for a in cys.atoms:
             if a.name in d:
                 a._charges['Kollman'] = d[a.name]
             else:
                 a._charges['Kollman'] = 0.0
 
-        #correct total
-        #newcharge = Numeric.sum(cys.atoms._charges['Kollman'])
+        # correct total
+        # newcharge = Numeric.sum(cys.atoms._charges['Kollman'])
         newcharge = 0
         for at in cys.atoms:
             newcharge = newcharge + at._charges['Kollman']
-
-
-
