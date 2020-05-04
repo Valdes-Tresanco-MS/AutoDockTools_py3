ddfc9abb329d394b070649097ac43ebe749a0810
MARIO <mariosergiovaldes145@gmail.com>
2020-05-04 03:27:32 -0500

    updated MolKit > pdb2pqr > extensions > phi.py

--- MolKit/pdb2pqr/extensions/phi.py
+++ MolKit/pdb2pqr/extensions/phi.py
@@ -1,3 +1,18 @@
+# ##################################################################################################
+#  Disclaimer                                                                                      #
+#  This file is a python3 translation of AutoDockTools (v.1.5.7)                                   #
+#  Modifications made by Valdes-Tresanco MS (https://github.com/Valdes-Tresanco-MS)                #
+#  Tested by Valdes-Tresanco-MS and Valdes-Tresanco ME                                             #
+#  There is no guarantee that it works like the original distribution,                             #
+#  but feel free to tell us if you get any difference to correct the code.                         #
+#                                                                                                  #
+#  Please use this cite the original reference.                                                    #
+#  If you think my work helps you, just keep this note intact on your program.                     #
+#                                                                                                  #
+#  Modification date: 4/5/20 3:27                                                                  #
+#                                                                                                  #
+# ##################################################################################################
+
 """
     Psi extension
 
@@ -11,28 +26,14 @@
 __date__ = "17 February 2006"
 __author__ = "Mike Bradley, Todd Dolinsky"
 
-# ##################################################################################################
-#  Disclaimer                                                                                      #
-#  This file is a python3 translation of AutoDockTools (v.1.5.7)                                   #
-#  Modifications made by Valdes-Tresanco MS (https://github.com/Valdes-Tresanco-MS)                #
-#  Tested by Valdes-Tresanco-MS and Valdes-Tresanco ME                                             #
-#  There is no guarantee that it works like the original distribution,                             #
-#  but feel free to tell us if you get any difference to correct the code.                         #
-#                                                                                                  #
-#  Please use this cite the original reference.                                                    #
-#  If you think my work helps you, just keep this note intact on your program.                     #
-#                                                                                                  #
-#  Modification date: 28/8/19 4:40                                                                 #
-#                                                                                                  #
-# ##################################################################################################
+from ..src.utilities import *
 
-from src.utilities import *
-from src.routines import *
 
 def usage():
-    str  = "        --phi         :  Print the per-residue backbone phi\n"
-    str += "                         angle to {output-path}.phi\n"
-    return str
+    text = "        --phi         :  Print the per-residue backbone phi\n"
+    text += "                         angle to {output-path}.phi\n"
+    return text
+
 
 def phi(routines, outroot):
     """
@@ -45,31 +46,38 @@ def phi(routines, outroot):
 
     outname = outroot + ".phi"
     file = open(outname, "w")
-    
+
     routines.write("\nPrinting phi angles for each residue...\n")
     routines.write("Residue     Phi\n")
     routines.write("----------------\n")
-    
+
     # Initialize some variables
 
     protein = routines.protein
 
     for residue in protein.getResidues():
         try:
-            if residue.peptideC != None:
+            if residue.peptideC is not None:
                 pepcoords = residue.peptideC.getCoords()
-            else: continue
-        except AttributeError: # Non amino acids
+            else:
+                continue
+        except AttributeError:  # Non amino acids
             continue
-        
-        if residue.hasAtom("N"): ncoords = residue.getAtom("N").getCoords()
-        else: continue
 
-        if residue.hasAtom("CA"): cacoords = residue.getAtom("CA").getCoords()
-        else: continue
+        if residue.hasAtom("N"):
+            ncoords = residue.getAtom("N").getCoords()
+        else:
+            continue
 
-        if residue.hasAtom("C"): ccoords = residue.getAtom("C").getCoords()
-        else: continue
+        if residue.hasAtom("CA"):
+            cacoords = residue.getAtom("CA").getCoords()
+        else:
+            continue
+
+        if residue.hasAtom("C"):
+            ccoords = residue.getAtom("C").getCoords()
+        else:
+            continue
 
         phi = getDihedral(pepcoords, ncoords, cacoords, ccoords)
         routines.write("%s\t%.4f\n" % (residue, phi))
