b6818a5710c9f5372e6b500bfce06241cf5188a9
MARIO <mariosergiovaldes145@gmail.com>
2020-05-10 17:36:16 -0500

    updated mglutil > util

---
+++ mglutil/util/__init__.py
@@ -0,0 +1,14 @@
+# ##################################################################################################
+#  Disclaimer                                                                                      #
+#  This file is a python3 translation of AutoDockTools (v.1.5.7)                                   #
+#  Modifications made by Valdes-Tresanco MS (https://github.com/Valdes-Tresanco-MS)                #
+#  Tested by Valdes-Tresanco-MS and Valdes-Tresanco ME                                             #
+#  There is no guarantee that it works like the original distribution,                             #
+#  but feel free to tell us if you get any difference to correct the code.                         #
+#                                                                                                  #
+#  Please use this cite the original reference.                                                    #
+#  If you think my work helps you, just keep this note intact on your program.                     #
+#                                                                                                  #
+#  Modification date: 10/5/20 17:35                                                                #
+#                                                                                                  #
+# ##################################################################################################
