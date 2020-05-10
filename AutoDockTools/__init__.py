9497d6a3c9b9f7f338632bae3e579d4127a9ac48
MARIO <mariosergiovaldes145@gmail.com>
2020-05-10 17:51:42 -0500

    updated AutoDockTools module

--- AutoDockTools/__init__.py
+++ AutoDockTools/__init__.py
@@ -1,3 +1,18 @@
+# ##################################################################################################
+#  Disclaimer                                                                                      #
+#  This file is a python3 translation of AutoDockTools (v.1.5.7)                                   #
+#  Modifications made by Valdes-Tresanco MS (https://github.com/Valdes-Tresanco-MS)                #
+#  Tested by Valdes-Tresanco-MS and Valdes-Tresanco ME                                             #
+#  There is no guarantee that it works like the original distribution,                             #
+#  but feel free to tell us if you get any difference to correct the code.                         #
+#                                                                                                  #
+#  Please use this cite the original reference.                                                    #
+#  If you think my work helps you, just keep this note intact on your program.                     #
+#                                                                                                  #
+#  Modification date: 10/5/20 17:50                                                                #
+#                                                                                                  #
+# ##################################################################################################
+
 #############################################################################
 #
 # Author: Ruth HUEY, Michel F. SANNER
@@ -20,7 +35,7 @@ import socket
 split = str.split
 
 from Support.version import __version__
-from mglutil import __revision__
+# from mglutil import __revision__
 
 # create hostDict with hostMacros accessible by anyone
 # from AutoDockTools.adthosts import hostMacros
